b35d9bd99a398120beb2b81da637a82a7ee4dc68
MalejandroC <macarrion44@utpl.edu.ec>
2025-01-23 10:44:16 -0500

    cambios

--- EjemplosExcepciones/src/manejoexepciones/EJerciciAnuncio.java
+++ EjemplosExcepciones/src/manejoexepciones/EJerciciAnuncio.java
@@ -21,30 +21,58 @@ public class EJerciciAnuncio {
         int num1 = 0;
         int num2 = 0;
         int div = 0;
+        boolean bandera = true;
 
         /*Realizar un proceso repetitivo que permita realizar la división de 
         números ingresados por teclado; el resultado de cada división debe ir 
         almacenandose en cada posición del arreglo. Considerar las excepciones
         posibles*/
-        try {
-            for (int i = 0; i < resultados.length; i++) {
-                System.out.println("Ingrese un valor");
-                num1 = entrada.nextInt();
-                System.out.println("Ingrese un valor");
-                num2 = entrada.nextInt();
+        while (bandera) {
+            try {
+                for (int i = 0; i < resultados.length; i++) {
+                    System.out.println("Ingrese un valor");
+                    num1 = entrada.nextInt();
+                    System.out.println("Ingrese un valor");
+                    num2 = entrada.nextInt();
 
-                div = num1 / num2;
+                    div = num1 / num2;
 
-                resultados[i] = div;
+                    resultados[i] = div;
+                    
+                    
+
+                    System.out.printf("RESULTADO: %d\n", resultados[i]);
+                    
+                }
+                
+                for (int i = 0; i < resultados.length; i++) {
+                    System.out.printf("RESULTADOS EN CADA POSICION: %d\n", resultados[i]);
+                }
+                
+                
+                bandera = false;
+                //se divide para cero
+            } catch (ArithmeticException e) {
+                System.out.printf("(ArithmeticException) Ocurrió una "
+                        + "excepción por dividir para 0\n");
+                
+                entrada.nextLine();
+
+                //se ingresa una letra
+            } catch (InputMismatchException e) {
+                System.out.printf("(InputMismatchException) Ocurrió una "
+                        + "excepción %s\n", e);
+                entrada.nextLine();
+                // GLOBAL 
+            } catch (Exception e) {
+                System.out.printf("Ocurrió una excepción %s\n", e);
+                entrada.nextLine();
+
             }
-
-        } catch (ArithmeticException e) {
-            System.out.printf("(ArithmeticException) Ocurrió una "
-                    + "excepción por dividir para 0\n");
-        } catch (InputMismatchException e) {
-            System.out.printf("(InputMismatchException) Ocurrió una "
-                    + "excepción %s\n", e);
+            
         }
+        
+        
 
     }
 
